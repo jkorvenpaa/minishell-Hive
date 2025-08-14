0ca5062b57ac2645cb3f1bc4be93bccafd30a13f
Jenni Korvenpaa <jkorvenp@c2r5p1.hive.fi>
2025-08-14 18:18:05 +0300

    added arena

---
+++ src/parser/arena.c
@@ -0,0 +1,39 @@
+
+#include "arena.h"
+
+//init area before using, returns arena
+mem_arena	*arena_init()
+{
+	mem_arena	*arena;
+	arena = malloc(sizeof(mem_arena));
+	if (!arena)
+		return(NULL);
+	arena->size = A_SIZE;
+	arena->buffer = malloc(arena->size);
+	if (!arena->buffer)
+	{
+		free (arena);
+		return(NULL);
+	}
+	arena->offset = 0;
+	return (arena);
+}
+
+//returns a pointer to arena where, alloc starts.
+//Moves offset for the amouth of bytes reserved
+void	*arena_alloc(mem_arena *arena, size_t bytes)
+{
+	void	*ptr;
+
+	if (arena->offset + bytes > arena->size)
+		return(NULL);
+	ptr = arena->buffer + arena->offset;
+	arena->offset = arena->offset + bytes;
+	return (ptr);
+}
+
+void	free_arena(mem_arena *arena)
+{
+	free(arena->buffer);
+	free(arena);
+}
