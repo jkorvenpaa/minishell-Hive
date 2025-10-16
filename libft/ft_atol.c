039ee1fda805a42a6b07a05a25e204a97c0507de
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2025-10-16 10:55:46 +0300

    fix: exit codes working

--- libft/ft_atol.c
+++ libft/ft_atol.c
@@ -6,33 +6,59 @@
 /*   By: nmascaro <nmascaro@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 15:57:18 by nmascaro          #+#    #+#             */
-/*   Updated: 2025/10/13 15:58:54 by nmascaro         ###   ########.fr       */
+/*   Updated: 2025/10/16 10:45:10 by nmascaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long	ft_atol(const char *nptr)
+static int	check_overflow(unsigned long long res, int digit, int sign)
 {
-	int		sign;
-	long	result;
-	int		i;
+	if (res > (9223372036854775807ULL - digit) / 10)
+		return (1);
+	if ((sign == 1 && res > 9223372036854775807ULL)
+		|| (sign == -1 && res > 9223372036854775808ULL))
+		return (1);
+	return (0);
+}
+
+static int	handle_sign(const char *str, int *i)
+{
+	int	sign;
 
 	sign = 1;
+	if (str[*i] == '-' || str[*i] == '+')
+	{
+		if (str[*i] == '-')
+			sign = -1;
+		(*i)++;
+	}
+	return (sign);
+}
+
+long long	ft_atol(const char *nptr, int *overflow)
+{
+	int					sign;
+	unsigned long long	result;
+	int					i;
+	int					digit;
+
 	result = 0;
 	i = 0;
+	*overflow = 0;
 	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if (nptr[i] == '-')
-			sign = -1;
-		i++;
-	}
+	sign = handle_sign(nptr, &i);
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		result = 10 * result + (nptr[i] - '0');
+		digit = nptr[i] - '0';
+		if (check_overflow(result, digit, sign))
+		{
+			*overflow = 1;
+			return (0);
+		}
+		result = 10 * result + digit;
 		i++;
 	}
-	return (result * sign);
+	return (sign * (long long)result);
 }
--- src/execution/built_ins_exit.c
+++ src/execution/built_ins_exit.c
@@ -6,7 +6,7 @@
 /*   By: nmascaro <nmascaro@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 11:11:35 by nmascaro          #+#    #+#             */
-/*   Updated: 2025/10/13 16:00:08 by nmascaro         ###   ########.fr       */
+/*   Updated: 2025/10/16 10:53:58 by nmascaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ static int	exit_isdigit(char *num)
 	int	i;
 
 	i = 0;
-	while (num[i] == '-' || num[i] == '+')
+	if (num[i] == '-' || num[i] == '+')
 		i++;
-	if (!num[i])
+	if (!num[i] || num[i] == '-' || num[i] == '+')
 		return (0);
 	while (num[i])
 	{
@@ -32,19 +32,24 @@ static int	exit_isdigit(char *num)
 
 static int	exit_arguments(t_command *command)
 {
+	int			overflow;
+	long long	num;
+
 	if (exit_isdigit(command->argv[1]) == 1)
 	{
-		if (!command->argv[2])
-			return (ft_atol(command->argv[1]));
-		else
+		if (ft_strncmp(command->argv[1], "-9223372036854775808", 21) == 0)
+			return (0);
+		num = ft_atol(command->argv[1], &overflow);
+		if (!command->argv[2] && !overflow)
+			return ((unsigned char)num);
+		else if (!overflow)
 		{
 			printf("exit: too many arguments\n");
 			return (-1);
 		}
 	}
-	else
-		printf("exit: %s: numeric argument required\n", command->argv[1]);
-	return (0);
+	printf("exit: %s: numeric argument required\n", command->argv[1]);
+	return (2);
 }
 
 void	exit_builtin(t_shell *shell, t_command *command)
@@ -54,15 +59,13 @@ void	exit_builtin(t_shell *shell, t_command *command)
 	e = 0;
 	if (command->argv[1])
 		e = exit_arguments(command);
+	else
+		e = shell->exit_status;
 	if (e == -1)
 	{
 		shell->exit_status = 1;
 		return ;
 	}
-	if (e == 0 && !command->argv[1])
-		e = shell->exit_status;
-	if (e == 0 && command->argv[1])
-		e = 2;
 	printf("exit\n");
 	close (shell->fd_in);
 	free_arena(shell->env_arena);
